bb3b1e489be09efc1f0e75e8d830f15f2644a8d1
amanaggarwal1 <amanaggarwal014@gmail.com>
2019-08-16 20:50:35 +0530

    Initial commit- Basic main xml file which shows the face of the app

---
+++ app/src/main/java/com/amanaggarwal1/bitcoin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.amanaggarwal1.bitcoin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main_activity);
+    }
+}
